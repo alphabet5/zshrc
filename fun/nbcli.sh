9b4e03aee61b3a6c105ac66e4ff56eafb3d547a3
alphabet5 <1178293+alphabet5@users.noreply.github.com>
2024-09-25 14:51:43 -0500

    Add some elastic functions.

--- fun/nbcli.sh
+++ fun/nbcli.sh
@@ -1,3 +1,3 @@
 nb () {
-  nbcli filter device $1 --json | jq -r '["NAME", "ENV", "PURPOSE", "BMC"],(.[] | [.name, .custom_fields.environment, .custom_fields.purpose,.custom_fields.bmc_ip4]) | @tsv' | python3 $MYDIR/python/prettytable.py 
+  nbcli filter device $1 --json | jq -r '["NAME", "ENV", "PURPOSE", "BMC", "PLATFORM"],(.[] | [.name, .custom_fields.environment, .custom_fields.purpose,.custom_fields.bmc_ip4,.platform.name]) | @tsv' | python3 $MYDIR/python/prettytable.py 
 }
