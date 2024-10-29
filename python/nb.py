c22777ac807620739399c7b2de8d72465425aa3c
alphabet5 <1178293+alphabet5@users.noreply.github.com>
2024-10-29 15:38:22 -0500

    Add a patch command for netbox

--- python/nb.py
+++ python/nb.py
@@ -4,6 +4,7 @@ import sys
 import os
 import requests
 from joblib import Parallel, delayed
+import json
 
 
 def netbox(method="GET", path="", params={}, value=None):
@@ -24,59 +25,87 @@ def netbox(method="GET", path="", params={}, value=None):
     else:
         return None
 
+
 def get_device(name):
     params = {"name__ic": name}
     devices = netbox(path="/api/dcim/devices/", params=params).json()
     if devices["count"] == 0:
-        devices = netbox(path="/api/virtualization/virtual-machines/", params=params).json()
+        devices = netbox(
+            path="/api/virtualization/virtual-machines/", params=params
+        ).json()
     return devices
+
 
 if __name__ == "__main__":
 
     args = sys.argv
     all_devices = []
+    if args[1] != "patch":
+        output = Parallel(n_jobs=30, verbose=0, backend="threading")(
+            map(delayed(get_device), args)
+        )
 
-    output = Parallel(n_jobs=30, verbose=0, backend="threading")(
-        map(delayed(get_device), args)
-    )
+        for devices in output:
+            if devices["count"] > 0:
+                all_devices += devices["results"]
+        devices_table = [
+            [
+                "NAME",
+                "ENV",
+                "PURPOSE",
+                "PLATFORM",
+                "BMC",
+                "MODEL",
+                "PARENT",
+                "K8S CLUSTER",
+            ]
+        ]
+        for device in all_devices:
+            row = list()
+            row.append(device["name"])
+            row.append(device["custom_fields"]["environment"])
+            row.append(device["custom_fields"]["purpose"])
+            try:
+                row.append(device["platform"]["name"])
+            except:
+                row.append(None)
+            if "virtualization" in device["url"]:
+                row.append(None)
+                row.append("VM")
+                row.append(None)
+            else:
+                row.append(device["custom_fields"]["bmc_ip4"])
+                row.append(device["device_type"]["display"])
+                row.append(device["parent_device"]["display"])
+            row.append(device["custom_fields"]["k8s_cluster"])
+            devices_table.append(row)
 
-    for devices in output:
-        if devices["count"] > 0:
-            all_devices += devices["results"]
-    devices_table = [
-        ["NAME", "ENV", "PURPOSE", "PLATFORM", "BMC", "MODEL", "PARENT", "K8S CLUSTER"]
-    ]
-    for device in all_devices:
-        row = list()
-        row.append(device["name"])
-        row.append(device["custom_fields"]["environment"])
-        row.append(device["custom_fields"]["purpose"])
-        try:
-            row.append(device["platform"]["name"])
-        except:
-            row.append(None)
-        if 'virtualization' in device['url']:
-            row.append(None)
-            row.append("VM")
-            row.append(None)
-        else:
-            row.append(device["custom_fields"]["bmc_ip4"])
-            row.append(device["device_type"]["display"])
-            row.append(device["parent_device"]["display"])
-        row.append(device["custom_fields"]["k8s_cluster"])
-        devices_table.append(row)
-
-    # data = sys.stdin.read()
-    table = Table(
-        box=None,
-    )
-    header = False
-    for row in devices_table:
-        if not header:
-            for col in row:
-                table.add_column(col)
-            header = True
-        else:
-            table.add_row(*row)
-    console = Console()
-    console.print(table)
+        # data = sys.stdin.read()
+        table = Table(
+            box=None,
+        )
+        header = False
+        for row in devices_table:
+            if not header:
+                for col in row:
+                    table.add_column(col)
+                header = True
+            else:
+                table.add_row(*row)
+        console = Console()
+        console.print(table)
+    elif args[1] == "patch":
+        patch = json.loads(args[-1])
+        for host in range(2, len(args) - 1):
+            try:
+                print(f"Patching {args[host]} with {args[-1]}")
+                device = netbox(
+                    path=f"/api/dcim/devices/", params={"name__ic": args[host]}
+                ).json()
+                id = device["results"][0]["id"]
+                result = netbox(
+                    method="PATCH", path=f"/api/dcim/devices/{id}/", value=patch
+                )
+                print(f"{args[host]}: {result.status_code}")
+            except:
+                print(f"{args[host]}: Error")
