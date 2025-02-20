a7e579fea10b4401637aeacd46dd1c7e6a9174e0
alphabet5 <1178293+alphabet5@users.noreply.github.com>
2025-02-20 11:13:52 -0600

    Add a delay to run-commands and improve the docker search function

--- fun/docker.sh
+++ fun/docker.sh
@@ -2,8 +2,13 @@ function ds() {
   function search() {
     echo "looking for ${1}..."
     NAME=$(curl -s "${DOCKER_REGISTRY}/v2/_catalog?n=1000" | jq -r '.repositories[]' | grep "${1}")
-    echo "${NAME}:"
-    curl -s "${DOCKER_REGISTRY}/v2/${NAME}/tags/list" | jq -r '.tags[]' | sort
+    while IFS= read -r line; do
+        echo "$line"
+        tags=$(curl -s "${DOCKER_REGISTRY}/v2/${line}/tags/list" | jq -r '.tags[]' | sort)
+        while IFS= read -r t; do
+            echo "  $t"
+        done <<< "$tags"
+    done <<< "$NAME"
   }
 
   if [ $# -eq 0 ]; then
